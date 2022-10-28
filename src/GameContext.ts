f94269d7b57e293df31581035cc5e0dbf5f61d9e
Patrick Canfield <pscale01@gmail.com>
2022-10-27 17:35:29 -0700

    dev: auto-format in pre-commit hook

--- src/GameContext.ts
+++ src/GameContext.ts
@@ -1,11 +1,11 @@
 import { EntityFactory } from './entities'
 import { Dict } from './types'
-import {World} from './World'
+import { World } from './World'
 
 export type GameContext = {
   audioContext: AudioContext
   deltaTime: number
   entityFactory: Dict<EntityFactory>
-  videoContext: CanvasRenderingContext2D,
+  videoContext: CanvasRenderingContext2D
   world: World
 }
