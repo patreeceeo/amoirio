6db39d6cdfde3c87555b88420270e0f75c37e461
Patrick Canfield <pscale01@gmail.com>
2022-11-14 16:11:04 -0800

    add event arg type checking

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,16 +1,34 @@
-type Callback = (...args: any[]) => void
-type Listener = { name: string | symbol; callback: Callback }
+import { Entity } from './EntityFunctions'
+import { ControlSignalType, ControlSignalState } from './input/InputSystem'
+import { Side } from './Entity'
+import { TileResolverMatch } from './TileResolver'
 
 export enum EventName {
   WORLD_INIT = 'world/init',
   WORLD_FIXED_STEP = 'world/fixed_step',
   WORLD_PAUSE = 'world/pause',
   WORLD_PLAY = 'world/play',
+  /** TODO delete? */
   TIMER_HURRY = 'timer/hurry',
+  /** TODO delete? */
   TIMER_OK = 'timer/ok',
   OBSTRUCT = 'obstruct',
   INPUT = 'input',
 }
+
+export type EventArgs = {
+  [EventName.INPUT]: [Array<Entity>, ControlSignalType, ControlSignalState]
+  [EventName.OBSTRUCT]: [Entity, Side, TileResolverMatch]
+  [EventName.WORLD_INIT]: []
+  [EventName.WORLD_FIXED_STEP]: []
+  [EventName.WORLD_PAUSE]: []
+  [EventName.WORLD_PLAY]: []
+  [EventName.TIMER_OK]: []
+  [EventName.TIMER_HURRY]: []
+}
+
+type Callback = (...args: any[]) => void
+type Listener = { name: string | symbol; callback: Callback }
 
 export class EventEmitter {
   private listeners: Listener[] = []
@@ -19,7 +37,7 @@ export class EventEmitter {
     this.listeners.push({ name, callback })
   }
 
-  emit(name: string | symbol, ...args: any[]) {
+  emit<N extends EventName>(name: N, ...args: EventArgs['input']) {
     this.listeners
       .filter((it) => it.name === name)
       .forEach((it) => it.callback(...args))
