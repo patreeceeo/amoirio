b83d4b69b99e58656eb80004f37f9dedba3bf478
Patrick Canfield <pscale01@gmail.com>
2022-11-18 19:31:32 -0800

    fix route / cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { loadEntities, EntityFactory } from './entities'
 import { GameContext } from './GameContext'
-import { createColorLayer } from './layers/color'
-import { createDashboardLayer } from './layers/dashboard'
-import { createPlayerProgressLayer } from './layers/player-progress'
-import { createTextLayer } from './layers/text'
 import { createLevelLoader } from './loaders/level'
-import { createPlayerEnv, makePlayer } from './player'
 import { raise } from './raise'
-import { Scene } from './Scene'
 import { SceneRunner } from './SceneRunner'
-import { TimedScene } from './TimedScene'
 import { Timer } from './Timer'
-import { DeprecatedEntity } from './Entity'
-import { Level } from './Level'
 import { Dict } from './types'
 import { World, WorldState } from './World'
 import { AudioSystem } from './audio/AudioSystem'
@@ -28,7 +19,6 @@ import {
 import { TraitSystem } from './traits/TraitSystem'
 import { InputSystem } from './input/InputSystem'
 import { TimerSystem } from './TimerSystem'
-import { loadFont } from './loaders/font'
 import { SpawnSystem } from './SpawnSystem'
 import { ScoreKeeper } from './ScoreKeeper'
 
@@ -40,12 +30,6 @@ function getVideoContext(canvas: HTMLCanvasElement): CanvasRenderingContext2D {
   videoContext.imageSmoothingEnabled = false
 
   return videoContext
-}
-
-function spawnMario(mario: DeprecatedEntity, level: Level) {
-  mario.pos.set(220, 0)
-  mario.vel.set(0, 0)
-  level.entities.add(mario)
 }
 
 async function createLoop(
@@ -98,68 +82,6 @@ async function startGame(canvas: HTMLCanvasElement) {
   const world = new World()
   const videoContext = getVideoContext(canvas)
 
-  const audioContext = new AudioContext()
-
-  const [entityFactory, font] = await Promise.all([
-    loadEntities(audioContext),
-    loadFont(),
-  ])
-
-  world.prefabs = entityFactory
-
-  const loadLevel = createLevelLoader(entityFactory)
-
-  const sceneRunner = new SceneRunner()
-
-  // const [_, mario] = entityFactory.mario?.() || raise('where mario tho')
-  // makePlayer(mario, 'MARIO')
-
-  // const inputRouter = setupKeyboard(window)
-  // inputRouter.addReceiver(mario)
-
-  const loadScreen = new Scene()
-  loadScreen.compositor.layers.push(createColorLayer('black'))
-  loadScreen.compositor.layers.push(createTextLayer(font, `LOADING ${name}...`))
-  sceneRunner.addScene(loadScreen)
-  sceneRunner.runNext()
-
-  await new Promise((resolve) => setTimeout(resolve, 500))
-
-  const level = await loadLevel('1-1')
-
-  const playerProgressLayer = createPlayerProgressLayer(font, level)
-  const dashboardLayer = createDashboardLayer(font, level)
-
-  // spawnMario(mario, level)
-
-  const playerEnv = createPlayerEnv(mario)
-  level.entities.add(playerEnv)
-
-  const waitScreen = new TimedScene()
-  waitScreen.compositor.layers.push(createColorLayer('black'))
-  waitScreen.compositor.layers.push(dashboardLayer)
-  waitScreen.compositor.layers.push(playerProgressLayer)
-  sceneRunner.addScene(waitScreen)
-
-  level.compositor.layers.push(dashboardLayer)
-  sceneRunner.addScene(level)
-
-  sceneRunner.runNext()
-
-  const loop = await createLoop(
-    videoContext,
-    audioContext,
-    entityFactory,
-    sceneRunner,
-    world,
-  )
-  loop.start()
-}
-
-async function startEditor(canvas: HTMLCanvasElement) {
-  const world = new World()
-  const videoContext = getVideoContext(canvas)
-
   // TODO should audioContext be instantiate in AudioSystem?
   const audioContext = new AudioContext()
 
@@ -176,28 +98,6 @@ async function startEditor(canvas: HTMLCanvasElement) {
   updateEntity(scoreKeeper, {
     [ComponentName.SCORE]: new ScoreKeeper(),
   })
-  // const editor = new Editor(level, world)
-
-  // const [_, mario] = entityFactory.mario?.() || raise('where mario tho')
-  // makePlayer(mario, 'MARIO')
-
-  // const [__, bullet] = entityFactory.goomba?.() || raise('where bullet tho')
-  // bullet.pos.set(24, 12)
-
-  // const inputRouter = setupKeyboard(window)
-
-  // inputRouter.addReceiver(mario)
-  // inputRouter.addReceiver(editor)
-
-  // spawnMario(mario, level)
-
-  // const playerEnv = createPlayerEnv(mario)
-  // level.entities.add(playerEnv)
-
-  // TODO add hot key to toggle this
-  // level.comp.layers.push(createCollisionLayer(level))
-
-  // level.compositor.layers.push(editorLayer)
   sceneRunner.addScene(level)
   level.pause()
 
@@ -218,10 +118,8 @@ if (canvas instanceof HTMLCanvasElement) {
   const path = location.pathname
   if (path === '/test') {
     runTests()
-  } else if (path === '/editor') {
+  } else if (path === '/') {
     runTests()
-    startEditor(canvas).catch(console.error)
-  } else {
     startGame(canvas).catch(console.error)
   }
 } else {
